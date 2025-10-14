0da4b08540f2009494fcc00f35e79ea57d832891
Scott <dkudviklerspg@gmail.com>
2025-10-15 01:55:38 +0200

    fsd

--- next.config.js
+++ next.config.js
@@ -6,30 +6,13 @@ const nextConfig = {
     unoptimized: true,
     domains: ['upload.wikimedia.org'],
   },
-  // Completely disable SWC to avoid download issues
+  // Disable SWC minification
   swcMinify: false,
-  compiler: {
-    removeConsole: false,
-  },
-  // Disable all experimental features
+  // Disable experimental features
   experimental: {},
   // Environment variables
   env: {
     NEXT_TELEMETRY_DISABLED: '1',
-  },
-  // Force use of Babel instead of SWC
-  webpack: (config, { isServer }) => {
-    // Disable SWC completely
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-    return config;
   },
 }
 
