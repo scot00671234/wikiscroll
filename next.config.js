7730c62bd1641df28edef4bb2e7926a58742b284
Scott <dkudviklerspg@gmail.com>
2025-10-15 02:04:47 +0200

    fe

--- next.config.js
+++ next.config.js
@@ -6,12 +6,14 @@ const nextConfig = {
     unoptimized: true,
     domains: ['upload.wikimedia.org'],
   },
-  // Disable SWC minification for older Next.js
+  // Disable SWC minification to avoid download issues
   swcMinify: false,
   // Environment variables
   env: {
     NEXT_TELEMETRY_DISABLED: '1',
   },
+  // Disable experimental features that might cause issues
+  experimental: {},
 }
 
 module.exports = nextConfig
