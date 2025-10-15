dfa4f761343beead9b1250d016a338b8c764933b
Scott <dkudviklerspg@gmail.com>
2025-10-15 02:16:09 +0200

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
@@ -101,7 +98,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={`${inter.className} bg-gray-50`}>
+      <body className="bg-gray-50 font-sans">
         {children}
       </body>
     </html>
